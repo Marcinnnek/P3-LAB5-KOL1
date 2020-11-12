f99649641d83826dd1030c75e02d36b679c5a098
Marcin Zuziak <porda1995@gmail.com>
2020-11-12 21:02:32 +0100

    start

---
+++ P3 LAB5 KOL1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P3_LAB5_KOL1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
